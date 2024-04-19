f97a203cbac852cc141498efe3d59855a338c3c9
Norbert Bondarczuk <nbondarczuk@yahoo.com>
2024-04-19 16:42:21 +0200

    Changed image naming convention

--- build/include/include.docker.mk
+++ build/include/include.docker.mk
@@ -21,13 +21,15 @@ PROJECT_OPTIONS=--project-directory=${PROJECT_DIR} \
 	--file ${PROJECT_DIR}/docker-compose-mongodb.yml \
 	--file ${PROJECT_DIR}/docker-compose-redis.yml
 
-docker/image:
-	DOCKER_BUILDKIT=1 docker build -f Dockerfile -t $(REPOSITORY)/$(TARGET):$(VERSION) . $(BUILD_ARGS)
-	docker tag $(REPOSITORY)/$(TARGET):$(VERSION) $(TARGET):latest
+IMAGE=$(REPOSITORY)/$(TARGET):$(VERSION)
+LATEST_IMAGE=$(REPOSITORY)/$(TARGET):latest
 
-docker/publish: committed
-	docker tag $(REPOSITORY)/$(TARGET):$(VERSION) $(TARGET):$(VERSION)
-	DOCKER_BUILDKIT=1 docker push $(REPOSITORY)/$(TARGET):$(VERSION)
+docker/image:
+	DOCKER_BUILDKIT=1 docker build -f Dockerfile -t $(IMAGE) . $(BUILD_ARGS)
+	docker tag $(REPOSITORY)/$(TARGET):$(VERSION) $(LATEST_IMAGE)
+
+docker/image/publish: committed
+	DOCKER_BUILDKIT=1 docker push $(LATEST_IMAGE)
 
 docker/clean:
 	docker rmi $(TARGET)
