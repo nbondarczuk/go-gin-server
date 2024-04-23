246032ae1111c4fe038fa6571f4082b0255170af
Norbert <nbondarczuk@yahoo.com>
2024-04-23 22:01:18 +0200

    mongo controller finished

--- internal/controller/entity/tag.go
+++ internal/controller/entity/tag.go
@@ -2,7 +2,9 @@ package entity
 
 import (
 	"context"
+	"time"
 
+	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
 
@@ -13,9 +15,11 @@ const TagCollectionName = "tag"
 
 // Tag is the entity mnaged by the controller.
 type Tag struct {
-	oid   primitive.ObjectID `json:"id" bson:"_id"`
-	label string             `json:"tabel" bson:"label"`
-	color string             `json:"color" bson:"color"`
+	oid       primitive.ObjectID `json:"id" bson:"_id"`
+	label     string             `json:"tabel" bson:"label"`
+	color     string             `json:"color" bson:"color"`
+	createdAt time.Time          `json:"created_at" bson:"created_at"`
+	updatedAt time.Time          `json:"updated_at" bson:"updated_at"`
 }
 
 // TagController is a container for resource accerss action state.
@@ -43,6 +47,7 @@ func NewTagController() (*TagController, error) {
 // Create an object with new oid allocated.
 func (tc *TagController) Create(tag *Tag) (*Tag, error) {
 	tag.oid = primitive.NewObjectID()
+	tag.createdAt = time.Now()
 	_, err := tc.collection.InsertOne(tc.ctx, tag)
 	if err != nil {
 		return nil, err
@@ -50,27 +55,54 @@ func (tc *TagController) Create(tag *Tag) (*Tag, error) {
 	return tag, nil
 }
 
-// Read an object by primary key.
-func (tc *TagController) Read(oid string) (*Tag, error) {
-	return nil, nil
+// ReadAll fetches a whole set of objects.
+func (tc *TagController) ReadAll() ([]Tag, error) {
+	cursor, err := tc.collection.Find(tc.ctx, bson.M{})
+	if err != nil {
+		return nil, err
+	}
+	defer cursor.Close(tc.ctx)
+	tags := make([]Tag, 0)
+	for cursor.Next(tc.ctx) {
+		var tag Tag
+		cursor.Decode(&tag)
+		tags = append(tags, tag)
+	}
+	return tags, nil
 }
 
-// ReadAll does the same on the whole collection.
-func (tc *TagController) ReadAll() ([]Tag, error) {
-	return nil, nil
+// ReadOne fetches one object by primary key.
+func (tc *TagController) ReadOne(oid string) (Tag, error) {
+	var tag Tag
+	err := tc.collection.FindOne(tc.ctx, bson.M{"_id": oid}).Decode(&tag)
+	if err != nil {
+		return Tag{}, err
+	}
+	return Tag{}, nil
 }
 
 // Update replaces all attributes of an existing object.
-func (tc *TagController) Update(tag *Tag) (*Tag, error) {
-	return nil, nil
-}
-
-// Patch replaces non null attributes of an existing object.
-func (tc *TagController) Patch(tag *Tag) error {
+func (tc *TagController) Update(oid string, tag *Tag) error {
+	updatedAt := time.Now()
+	_, err := tc.collection.UpdateOne(tc.ctx,
+		bson.M{"_id": oid},
+		bson.D{{"$set",
+			bson.D{
+				{"label", tag.label},
+				{"color", tag.color},
+				{"updated_at", updatedAt},
+			}}})
+	if err != nil {
+		return err
+	}
 	return nil
 }
 
 // Delete removes an object from collection using primary key.
 func (tc *TagController) Delete(oid string) error {
+	_, err := tc.collection.DeleteOne(tc.ctx, bson.M{"_id": oid})
+	if err != nil {
+		return err
+	}
 	return nil
 }
--- internal/handler/tag/tag.go
+++ internal/handler/tag/tag.go
@@ -8,7 +8,7 @@ import (
 	"go-gin-server/internal/controller/entity"
 )
 
-// CreateHandler makes a new resource with give attributes.
+// CreateHandler makes a new resource with given attributes.
 func CreateHandler(c *gin.Context) {
 	var tag entity.Tag
 	// Check input ie. new object attributes from request body.
@@ -41,15 +41,64 @@ func CreateHandler(c *gin.Context) {
 	return
 }
 
+// ReadHandler reaks one or all records from the controller.
 func ReadHandler(c *gin.Context) {
+	// The controlle gives access to particular collection.
+	tc, err := entity.NewTagController()
+	if err != nil {
+		// Handle error in controller allocation.
+		c.JSON(http.StatusInternalServerError,
+			gin.H{"error": err.Error()})
+		return
+	}
+	id := c.Param("id")
+	var rval any
+	// Dispatch in case the id was provided in the request.
+	if id != "" {
+		rval, err = tc.ReadOne(id)
+	} else {
+		rval, err = tc.ReadAll()
+	}
+	if err != nil {
+		// Handle error in controller read operation.
+		c.JSON(http.StatusInternalServerError,
+			gin.H{"error": err.Error()})
+		return
+	}
 	r := map[string]interface{}{
 		"Status": "Ok",
+		"Object": rval,
 	}
 	c.JSON(http.StatusOK, r)
 	return
 }
 
+// UpdateHandler replaces lla attributes of a given object.
 func UpdateHandler(c *gin.Context) {
+	// The controlle gives access to particular collection.
+	tc, err := entity.NewTagController()
+	if err != nil {
+		// Handle error in controller allocation.
+		c.JSON(http.StatusInternalServerError,
+			gin.H{"error": err.Error()})
+		return
+	}
+	id := c.Param("id")
+	if id == "" {
+		// Handle error in request without a parameter.
+		c.JSON(http.StatusBadRequest,
+			gin.H{"error": ErrEmptyTagId})
+		return
+	}
+	var tag entity.Tag
+	// Check input ie. new object attributes from request body.
+	if err := c.ShouldBindJSON(&tag); err != nil {
+		// Handle error in request body.
+		c.JSON(http.StatusBadRequest,
+			gin.H{"error": err.Error()})
+		return
+	}
+	err = tc.Update(id, &tag)
 	r := map[string]interface{}{
 		"Status": "Ok",
 	}
@@ -57,7 +106,30 @@ func UpdateHandler(c *gin.Context) {
 	return
 }
 
+// DeleteHandler removes an object from backend.
 func DeleteHandler(c *gin.Context) {
+	// The controller gives access to particular collection.
+	tc, err := entity.NewTagController()
+	if err != nil {
+		// Handle error in controller allocation.
+		c.JSON(http.StatusInternalServerError,
+			gin.H{"error": err.Error()})
+		return
+	}
+	id := c.Param("id")
+	if id == "" {
+		// Handle error in request without a parameter.
+		c.JSON(http.StatusBadRequest,
+			gin.H{"error": ErrEmptyTagId})
+		return
+	}
+	err = tc.Delete(id)
+	if err != nil {
+		// Handle error in object deleteing.
+		c.JSON(http.StatusInternalServerError,
+			gin.H{"error": err.Error()})
+		return
+	}
 	r := map[string]interface{}{
 		"Status": "Ok",
 	}
